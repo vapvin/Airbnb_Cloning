69d80f3ab021320746dbf902f1be9e17f87ff30d
vapvin <tpehdj12@daum.net>
2019-09-25 11:52:24 +0900

    Rooms Moels

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as users_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ RoomType Model Difinition """
+
+    pass
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Difinition """
+
+    pass
+
+
+class Facility(AbstractItem):
+    """ Facility Model Difinition """
+
+    pass
+
+
+class HouseRule(AbstractItem):
+    """ HouseRule Model Difinition """
+
+    pass
+
+
+class Room(core_models.TimeStampedModel):
+
+    """ Room Model Definition """
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(users_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
+
+    def __str__(self):
+        return self.name
