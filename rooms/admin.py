9f17b307bcb06654535879e786019169110fc4d2
vapvin <tpehdj12@daum.net>
2019-09-29 17:31:03 +0900

    Sunday After Study

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,11 +6,11 @@ from . import models
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
 
-    """ Item Admin Dfinition """
+    """ Item Admin Definition """
 
     list_display = ("name", "used_by")
 
-    def used_by(slef, obj):
+    def used_by(self, obj):
         return obj.rooms.count()
 
     pass
@@ -18,29 +18,26 @@ class ItemAdmin(admin.ModelAdmin):
 
 class PhotoInline(admin.TabularInline):
 
-    mpdel = models.Photo
+    model = models.Photo
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
-    """ Room Admin Dfinition """
+    """ Room Admin Definition """
 
-    inline = (PhotoInline,)
+    inlines = (PhotoInline,)
 
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")},
+            {"fields": ("name", "description", "country", "city", "address", "price")},
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
         (
             "More About the Space",
-            {
-                "classes": ("collapse",),
-                "fields": ("amenities", "facilities", "house_rules"),
-            },
+            {"fields": ("amenities", "facilities", "house_rules")},
         ),
         ("Last Details", {"fields": ("host",)}),
     )
@@ -61,8 +58,6 @@ class RoomAdmin(admin.ModelAdmin):
         "count_photos",
         "total_rating",
     )
-
-    ordering = ("name", "price", "bedrooms")
 
     list_filter = (
         "instant_book",
@@ -91,12 +86,11 @@ class RoomAdmin(admin.ModelAdmin):
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    """ Photo Admin Dfinition """
+    """ Phot Admin Definition """
 
     list_display = ("__str__", "get_thumbnail")
 
     def get_thumbnail(self, obj):
-        return mark_safe(f'<img width="50px" height="50px" src="{obj.file.url}" />')
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
 
     get_thumbnail.short_description = "Thumbnail"
-
