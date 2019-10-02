e71275f678a6f63b7a0225a2794e01217056cd8a
vapvin <tpehdj12@daum.net>
2019-10-02 14:56:00 +0900

    Using DetailView CBV

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "(fu$36pziv@f5-4+e=-)p=)o&6xj06m-$uznjk__=hwrdewko5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition
