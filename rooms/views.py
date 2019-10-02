2937eee676155820e715d876993627f2834aeb7e
vapvin <tpehdj12@daum.net>
2019-10-02 12:49:15 +0900

    URLs and Arguments

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,19 @@
-from math import ceil
+from django.views.generic import ListView
 from django.shortcuts import render
-from django.core.paginator import Paginator
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms})
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    parginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+
+    return render(request, "rooms/detail.html")
