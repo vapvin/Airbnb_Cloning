8fb83da42a09010aa72bed7f987eec28ae781674
vapvin <tpehdj12@daum.net>
2019-10-03 17:40:11 +0900

    Finishing the Form

--- rooms/views.py
+++ rooms/views.py
@@ -10,20 +10,20 @@ class HomeView(ListView):
 
     model = models.Room
     paginate_by = 10
-    parginate_orphans = 5
+    paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
 
 
 class RoomDetail(DetailView):
 
-    """ RommDetail Definition """
+    """ RoomDetail Definition """
 
     model = models.Room
 
 
 def search(request):
-    city = request.GET.get("city", "Anywher")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
@@ -32,8 +32,10 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    s_amenities = request.GET.get("amenities")
-    s_facilities = request.GET.get("facilities")
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
 
     form = {
         "city": city,
@@ -44,6 +46,10 @@ def search(request):
         "bedrooms": bedrooms,
         "beds": beds,
         "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
     }
 
     room_types = models.RoomType.objects.all()
@@ -58,4 +64,3 @@ def search(request):
     }
 
     return render(request, "rooms/search.html", {**form, **choices})
-
